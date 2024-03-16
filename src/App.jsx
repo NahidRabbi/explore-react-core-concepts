8ef2f640e9061bff295aef707c240dd65ea99278
nahid <nahidrabbi045@gmail.com>
2024-03-17 01:58:18 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import './App.css'
+import Counter from './counter';
+import Team from './Team';
+
+function App() {
+
+  function handleClick(){
+    alert('button clicked');
+  }
+  
+  const handleClick2 = () =>{
+    alert('button 2 clicked')
+  }
+ 
+  const addToFive = (num) =>{
+    alert(num + 5);
+  }
+
+return (
+    <>
+     <h3>React Core Concept 2</h3>
+
+     <Team></Team>
+
+     <Counter></Counter>
+
+     <button onClick={handleClick}>Click Me</button>
+     <button onClick={handleClick2}>click2</button>
+     <button onClick={() =>{alert('third clicked')}}>Third</button>
+     {/* vejailla button */}
+     <button onClick={() => addToFive(3)}>Four</button>
+    </>
+  )
+}
+
+export default App
